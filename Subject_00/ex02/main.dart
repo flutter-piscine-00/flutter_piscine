7e5ce11a7e5230a890cadd003b42681ad4f51a02
honlee <kij753@naver.com>
2021-12-02 18:49:02 +0900

    [test]

--- Subject_00/ex02/main.dart
+++ Subject_00/ex02/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
 // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { //
     return MaterialApp(
       title: 'Day00_Ex02',
       theme: ThemeData(
